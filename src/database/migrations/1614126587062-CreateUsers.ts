681e06291c67746538fbe77d85ab4ceb0bc9c520
Carlos Eduardo Ribeiro <carloseribeiro96@gmail.com>
2021-02-23 21:47:52 -0300

    Adicionado tabela users na migration;

--- src/database/migrations/1614126587062-CreateUsers.ts
+++ src/database/migrations/1614126587062-CreateUsers.ts
@@ -1,11 +1,41 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateUsers1614126587062 implements MigrationInterface {
 
+    // up() -> Cria a migration
     public async up(queryRunner: QueryRunner): Promise<void> {
+        // Cria a tabela users:
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "name",
+                        type: "varchar"
+                    },
+                    {
+                        name: "email",
+                        type: "varchar"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
     }
 
+    // down() -> Desfaz a migration
     public async down(queryRunner: QueryRunner): Promise<void> {
+        // Deleta a tabela users
+        await queryRunner.dropTable("users");
     }
 
 }
