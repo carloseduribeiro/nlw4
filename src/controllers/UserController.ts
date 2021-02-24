f3ad0f90f8ea966c3c8af076013ec786bdf2c4fd
Carlos Eduardo Ribeiro <carloseribeiro96@gmail.com>
2021-02-24 01:55:04 -0300

    Implementação do método create finalizada;

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,13 +1,32 @@
 import {Request, Response} from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../models/User';
 
 class UserController {
     async create(request: Request, response: Response) {
-        const body = request.body;
+        const { name, email } = request.body;
 
-        console.log(body);
+        // Cria o usersRepository:
+        const usersRepository = getRepository(User);
 
-        return response.send();
+        // Faz uma consulta pelo email informado:
+        const usersAlreadyExists = await usersRepository.findOne({ email });
 
+        // Verifica se já existe um usuário cadastrado com o email:
+        if (usersAlreadyExists) {
+            return response.status(400).json({
+                error: "User already exists!"
+            })
+        }
+
+        const user = usersRepository.create({
+            name,
+            email
+        });
+
+        await usersRepository.save(user);
+
+        return response.json(user);
     }
 }
 
