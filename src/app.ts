15ac37f4ee025647b175e547961f10a63cb08767
Carlos Eduardo Ribeiro <carloseribeiro96@gmail.com>
2021-02-24 20:53:45 -0300

    separado app para facilitar os testes;

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import 'reflect-metadata' ;
+import './database';    // Faz a conexão com o banco de dados sempre que a aplicação é iniciada;
+import express from 'express';
+import { router } from './routes';
+
+const app = express();
+
+app.use(express.json());    // Informa o express que o formato que será trabalhado. 
+app.use(router);
+
+export { app };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,3 @@
-import 'reflect-metadata' ;
-import './database';    // Faz a conexão com o banco de dados sempre que a aplicação é iniciada;
-import express from 'express';
-import { router } from './routes';
-
-const app = express();
-
-app.use(express.json());    // Informa o express que o formato que será trabalhado. 
-app.use(router);
+import { app } from "./app";
 
 app.listen(3333, () => console.log("Server is running!"));
